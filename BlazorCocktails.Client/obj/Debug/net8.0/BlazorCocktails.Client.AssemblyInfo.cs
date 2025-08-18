bc1fbd9a05fcee0d5bd33d6faa8259b94c3e493c
Isra8Rubio <isra-rubio@hotmail.com>
2025-08-18 14:47:48 +0200

    Paginacion Complete

--- BlazorCocktails.Client/obj/Debug/net8.0/BlazorCocktails.Client.AssemblyInfo.cs
+++ BlazorCocktails.Client/obj/Debug/net8.0/BlazorCocktails.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorCocktails.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f64af0a096cf63138d6c1ec38c6c1a3b97a7ba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a511df09241ebf91223377e7b5b871e100bccc76")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorCocktails.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorCocktails.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
