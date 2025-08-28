f27f34231255126b74a850ead8339def165aa01a
Isra8Rubio <isra-rubio@hotmail.com>
2025-08-28 12:54:04 +0200

    Entrega

--- BlazorCocktails.Client/obj/Debug/net8.0/BlazorCocktails.Client.AssemblyInfo.cs
+++ BlazorCocktails.Client/obj/Debug/net8.0/BlazorCocktails.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorCocktails.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+456e670a9304aa45564c13329ff0ad36bcd415bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc1265ca64cf7f30686138e250a2c036579fdb0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorCocktails.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorCocktails.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
